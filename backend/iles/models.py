2ea7339dbf0a55639d6a220eb4a2d37ca68112fb
beatrice <beatricenassali273@gmail.com>
2026-03-12 01:38:32 +0300

    Created Custom User model

--- backend/iles/models.py
+++ backend/iles/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    ROLES_CHOICES =[
+        ('student','Student'),
+        ('work supervisor','Work supervisor'),
+        ('university supervisor','University supervisor'),
+        ('admin','Admin')
+    ]
+    roles =models.CharField (
+        max_length= 30,
+        choices = ROLES_CHOICES,
+        default= 'student '
+  
+    )
+    def __str__(self):
+        return f"{self.username}-{self.roles}"
--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -83,6 +83,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'iles.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
