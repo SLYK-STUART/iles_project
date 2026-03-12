da0d2b8f68fa678f5d6ac80bf5a36825929ddcd8
GloboGo <stuartslyk@gmail.com>
2026-03-12 11:50:23 +0300

    Created different apps for different models

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'iles',
+    'patience',
+    'precious',
+    'stuart',
+    'acen',
 ]
 
 MIDDLEWARE = [
