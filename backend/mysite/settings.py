5a70fdb290dd1465d969fb0e7a38ba6f49bdc71a
GloboGo <stuartslyk@gmail.com>
2026-03-21 19:28:41 +0300

    alot of testing...

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iles',
+    'accounts',
     'patience',
     'precious',
     'stuart',
@@ -87,7 +87,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
-AUTH_USER_MODEL = 'iles.CustomUser'
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
