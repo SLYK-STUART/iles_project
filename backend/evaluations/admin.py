34b55e921fd220c49a8789e613239c007c70e05b
GloboGo <stuartslyk@gmail.com>
2026-03-25 23:13:21 +0300

    Initialized relationships

--- backend/evaluations/admin.py
+++ backend/evaluations/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import EvaluationCriteria, Evaluation
+from .models import EvaluationCriteria, Evaluation, EvaluationItem
 
 admin.site.register(EvaluationCriteria)
 admin.site.register(Evaluation)
+admin.site.register(EvaluationItem)
