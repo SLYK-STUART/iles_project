e1a9ddcb5b8cc0604a2270106b506788c5afae17
AkurutuPatience <akurutupatience@email.com>
2026-03-25 00:50:39 -1100

    created an app

---
+++ backend/placements/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import InternshipPlacement, Company
+
+# Register your models here.
+admin.site.register(InternshipPlacement)
+admin.site.register(Company)
