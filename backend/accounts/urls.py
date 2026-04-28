a0f016c08092be084114d5214dc687e6eec9f431
Stuart <slykstuart@gmail.com>
2026-04-28 10:34:07 +0300

    Updated accounts.urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import CustomLoginView, StudentDashboardView
+from .views import CustomLoginView, StudentDashboardView, AdminUserDetailView, AdminUserListView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view()),
     path('student/dashboard/', StudentDashboardView.as_view(), name='student-dashbaord'),
+
+    path('admin/users/', AdminUserListView.as_view(), name='admin-user-list'),
+    path('admin/users<int:user_id>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
 ]
