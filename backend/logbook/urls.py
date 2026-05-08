ffc2e00511dc2858ff09b6f376be42ed33ba4fe2
Stuart <slykstuart@gmail.com>
2026-05-08 14:19:28 +0300

    valid urls

--- backend/logbook/urls.py
+++ backend/logbook/urls.py
@@ -1,9 +1,15 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WeeklyLogViewSet, LogReviewViewSet
+from .views import WeeklyLogViewSet, LogReviewViewSet, AcademicDashboardView, AdminDashboardView, WorkplaceDashboardView
 
 router = DefaultRouter()
 
-router.register(r'weekly-logs', WeeklyLogViewSet, basename='weekly-logs')
-router.register(r'log-reviews', LogReviewViewSet, basename='log-reviews')
+router.register(r'logs', WeeklyLogViewSet, basename='logs')
+router.register(r'reviews', LogReviewViewSet, basename='reviews')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path("academic-dashboard/", AcademicDashboardView.as_view(), name='academic-dashboard'),
+    path("admin-dashboard/", AdminDashboardView.as_view(), name='admin-dashboard'),
+    path("wp-dashboard/", WorkplaceDashboardView.as_view())
+]
